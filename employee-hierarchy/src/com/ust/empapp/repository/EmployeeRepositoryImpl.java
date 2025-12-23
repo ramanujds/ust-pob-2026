ee5c0c7ec07067cae8c7598d3fca07911b938ab7
ramanujds <ramanujds9@gmail.com>
2025-12-23 11:21:45 +0530

    Arrays and String

--- employee-hierarchy/src/com/ust/empapp/repository/EmployeeRepositoryImpl.java
+++ employee-hierarchy/src/com/ust/empapp/repository/EmployeeRepositoryImpl.java
@@ -5,16 +5,35 @@ import com.ust.empapp.model.Employee;
 public class EmployeeRepositoryImpl{
 
     Employee []employees = new Employee[10];
+    int current = 0;
 
     public void saveEmployee(Employee employee) {
 
+        if (current<employees.length)
+            employees[current++]=employee;
+
     }
 
-    public void findEmployee(int id) {
+    public Employee findEmployee(int id) {
+
+        for (int i = 0; i < current ; i++) {
+            var emp = employees[i];
+            if (emp!=null && emp.getEmployeeId()==id){
+                return emp;
+            }
+        }
+        return null;
 
     }
 
     public void deleteEmployee(int id) {
+
+        for (int i = 0; i < current ; i++) {
+            var emp = employees[i];
+            if (emp!=null && emp.getEmployeeId()==id){
+                emp = null;
+            }
+        }
 
     }
 }
---
+++ java-programming-basics/src/basics/ArraysExample.java
@@ -0,0 +1,23 @@
+package basics;
+
+public class ArraysExample {
+
+
+    static void main() {
+
+        int []arr = new int[5];
+
+//        for (int i = 0; i <arr.length ; i++) {
+//            System.out.println(arr[i]);
+//
+//        }
+
+        for (var item:arr){
+            System.out.println(item);
+        }
+
+        String []strArr = new String[10];
+
+    }
+
+}
